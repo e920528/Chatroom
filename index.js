89a33497c76f0c2b689d27536faea582657e2cdc
YuWei Cheng <e920528@gmail.com>
2018-04-12 21:28:54 +0800

    final version

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
+//use express.js and socket.io
 const express = require('express');
 const app = express();
-// 加入這兩行
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
  
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
- 
-// 加入線上人數計數
-let onlineCount = 0;
 
-// 修改 connection 事件
+//require css file from /css
+app.use('/css', express.static('css'));
+ 
+// number of online people
+let onlineNum = 0;
+
+// connection event
 io.on('connection', (socket) => {
-    // 有連線發生時增加人數
-    onlineCount++;
-    // 發送人數給網頁
-    io.emit("online", onlineCount);
+    // once there's a new person, increase the onlineNum
+    onlineNum++;
+    // send onlineNum to html page
+    io.emit("online", onlineNum);
 
     socket.on("greet", () => {
-        socket.emit("greet", onlineCount);
+        socket.emit("greet", onlineNum);
     });
 
     socket.on('disconnect', () => {
-        // 有人離線了，扣人
-        onlineCount = (onlineCount < 0) ? 0 : onlineCount-=1;
-        io.emit("online", onlineCount);
+        // once there's a person disconnect, decrease onlineNum
+        // Notice that min of onlineNum should be 0
+        onlineNum = (onlineNum < 0) ? 0 : onlineNum-=1;
+        io.emit("online", onlineNum);
     });
     socket.on("send", (msg) => {
-        // 如果 msg 內容鍵值小於 2 等於是訊息傳送不完全
-        // 因此我們直接 return ，終止函式執行。
-        if (Object.keys(msg).length < 2) return;
-        // 廣播訊息到聊天室
-        io.emit("msg", msg);
+        //if message is not null, send message to chatroom
+        if(Object.keys(msg).length < 1)
+            io.emit("msg", msg);
     });
 });
  
-// 注意，這邊的 server 原本是 app
 server.listen(3000, () => {
     console.log("Server Started. http://localhost:3000");
 });
